ca773f0ae321a72d2edd27de28ce816fa45dcb92
samridhi8595-lab <samridhi8595@gmail.com>
2025-12-24 15:48:01 +0530

    basic uses of usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState(0)
+
+  function increaseNum(){
+    setNum(num+1)
+
+  }
+  function decreaseNum(){
+    setNum(num-1)
+
+  }
+  function jump5Num(){
+    setNum(num+5)
+  }
   return (
     <div>
-      
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>increase</button>
+      <button onClick={decreaseNum}>decrease</button>
+      <button onClick={jump5Num}>increase 5</button>
     </div>
   )
 }
